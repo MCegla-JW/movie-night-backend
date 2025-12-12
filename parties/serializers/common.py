cb3e268e51253dcfb4dd0d2b720b20a594c1375d
MCegla-JW <m.cegla93@gmail.com>
2025-12-12 15:00:03 +0000

    added add random movie from creator list to party logic

--- parties/serializers/common.py
+++ parties/serializers/common.py
@@ -1,9 +1,25 @@
-from rest_framework.serializers import ModelSerializer
-from parties.models import Party
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from parties.models import Party, PartyMovie
+from movies.models import Movie
+from users.models import User
 
 class PartySerializer(ModelSerializer):
     class Meta:
         model = Party
         fields = '__all__'
         read_only_fields = ['join_code', 'creator', 'members']
+
+
+class PartyMovieSerializer(ModelSerializer):
+        party = PrimaryKeyRelatedField(queryset=Party.objects.all())
+        movie = PrimaryKeyRelatedField(queryset=Movie.objects.all())
+        added_by_user = PrimaryKeyRelatedField(queryset=User.objects.all())
+
+        class Meta:
+             model = PartyMovie
+             fields = '__all__'
+
+
+
+        
     
--- parties/views.py
+++ parties/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Party, PartyMovie
 from rest_framework.exceptions import NotFound
-from .serializers.common import PartySerializer
+from .serializers.common import PartySerializer, PartyMovieSerializer
 from django.db.models import Q
 import uuid
+from movies.models import Watchlist
 
 # Create your views here.
 ## PARTY ROUTES 
@@ -30,10 +31,29 @@ class PartyIndex(APIView):
         serializer = PartySerializer(data=request.data)
         is_valid = serializer.is_valid(raise_exception=True)
         print(serializer._validated_data)
+        # save created party
         created_party = serializer.save(creator = request.user, join_code = uuid.uuid4())
+        # add creator as first party member 
         creator = request.user
         created_party.members.add(creator)
-        return Response(serializer.data, 201)
+        # add movie to party movie 
+        # get creators watchlist movies and filter and find one at random
+        movie_to_add = Watchlist.objects.filter(user = request.user.id).order_by("?").first()
+        if movie_to_add == None:
+            return Response({'message': 'Watchlist empty. Add movies first'})
+        print(movie_to_add)
+        # create a party movie instance 
+        # data from Party Movie model 
+        data = {
+        'party': created_party.id,
+        'movie': movie_to_add.movie.id,  
+        'added_by_user': creator.id
+        }
+        serializer_party_movie = PartyMovieSerializer(data=data)
+        is_valid = serializer_party_movie.is_valid(raise_exception=True)
+        party_movie = serializer_party_movie.save()
+        updated_serializer = PartySerializer(created_party)
+        return Response(updated_serializer.data, 201)
 
 
     # class PartyItemsView(APIView):
