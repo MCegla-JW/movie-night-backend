a9452cd48e5372fa12fe113c98e5c9dcde29070b
MCegla-JW <m.cegla93@gmail.com>
2025-12-17 18:09:39 +0000

    refactored PartySerializer to display movies

--- parties/serializers/common.py
+++ parties/serializers/common.py
@@ -6,29 +6,32 @@ from movies.serializers.common import MovieSerializer
 from rest_framework import serializers
 
 
+class PartyMovieSerializer(ModelSerializer):
+    party = PrimaryKeyRelatedField(queryset=Party.objects.all())
+    movie = MovieSerializer()
+    added_by_user = PrimaryKeyRelatedField(queryset=User.objects.all())
+    is_winner = serializers.SerializerMethodField(read_only=True)
+    num_votes = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = PartyMovie
+        fields = ['party', 'movie', 'added_by_user', 'is_winner', 'num_votes']
+
+    def get_is_winner(self, obj):
+        max_votes = self.context.get('max_votes', 0)
+        if max_votes == 0:
+            return False
+        return obj.num_votes == max_votes
+
+
 class PartySerializer(ModelSerializer):
     members = UserSerializer(many=True, read_only=True)
     creator = UserSerializer(read_only=True)
+    movies = PartyMovieSerializer(source='party_movies', many=True, read_only=True)
+
     class Meta:
         model = Party
         fields = '__all__'
         read_only_fields = ['join_code', 'creator', 'members']
-    
-class PartyMovieSerializer(ModelSerializer):
-        party = PrimaryKeyRelatedField(queryset=Party.objects.all())
-        movie = MovieSerializer()
-        added_by_user = PrimaryKeyRelatedField(queryset=User.objects.all())
-        is_winner = serializers.SerializerMethodField(read_only=True)
-        num_votes = serializers.IntegerField(read_only=True)
 
-        class Meta:
-             model = PartyMovie
-             fields = ['party', 'movie', 'added_by_user', 'is_winner', 'num_votes']
-        
-        def get_is_winner(self, obj):
-            # Votes per movie
-            max_votes = self.context.get('max_votes', 0)
-            if max_votes == 0:
-                 return False
-            return obj.num_votes == max_votes
 
